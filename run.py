1c231a5d98f400524a8c034bf20497007d90a0fd
davide laneri <davidelan@hotmail.com>
2024-03-21 14:50:45 +0000

    Add get_name() and main() functions

--- run.py
+++ run.py
@@ -12,9 +12,55 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('psychology-test')
 
-sales = SHEET.worksheet('sales')
+questionnaires = SHEET.worksheet('questionnaires')
 
-data = sales.get_all_values()
+#sales = SHEET.worksheet('sales')
 
-print(data)
+#data = sales.get_all_values()
 
+#print(data)
+
+def get_name():
+    """
+    Gets name from terminal, validates the input and prints greeting
+    using the name provided
+    """
+    while True:
+        try:
+            name = input("Please enter your name: ")
+
+            if not name:
+                raise ValueError("Name cannot be empty")
+
+            if " " in name:
+                raise ValueError("Name cannot contain spaces")
+
+            if not name.isalpha():
+                raise ValueError("Name can only contain letters")
+
+            if len(name) > 20:
+                raise ValueError("Name must be 20 characters or less")
+
+            if not name[0].isupper():
+                raise ValueError("Name must start with a capital letter")
+
+            else:
+                print("\n")
+                print("Hello " + name + ", Welcome to our psychology-test for fun\n")
+                break  # if name follows all rules, exit while loop
+
+        except ValueError as e:
+            print(e)
+
+    return name
+
+
+def main():
+    """
+    Run all program functions
+    """
+    name = get_name()
+ 
+    
+
+main()

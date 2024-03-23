bf930227b2aa755e24bbf928537865ae386c7f3f
davide laneri <davidelan@hotmail.com>
2024-03-23 17:33:21 +0000

    Arun_questionnaires() function

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import re #Regular expression operations
+from colorama import Fore # for producing colored terminal text and cursor positioning
+import os
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,6 +16,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('psychology-test')
 
+#sales = SHEET.worksheet('questionnaires')
 questionnaires = SHEET.worksheet('questionnaires')
 descriptions = SHEET.worksheet('descriptions')
 
@@ -73,6 +78,31 @@ def get_name():
     print("\n")
 
 
+    def choose_questionnaire():
+    while True:
+        print("Please choose a questionnaire:\n")
+        print("1. Questionnaire GAD-7. - 7 questions")
+        print("2. Questionnaire SAS-20. - 20 questions\n")
+        #print("\n")
+        choice = input("Enter 1 or 2: ")
+        
+        if choice in ['1']:
+            os.system('cls')  # Clears screen For Windows
+            print("\n")
+            print("You have chosen the questionnaire GAD-7\n")
+            print_description(gad7_description) # Prints the description of the GAD-7 questionnaire
+            return choice           
+        if choice in ['2']:
+            os.system('cls')  # Clears screen For Windows
+            print("\n")
+            print("You have chosen the questionnaire SAS-20\n")
+            print_description(sas20_description) # Prints the description of the SAS-20 questionnaire
+            return choice
+        else:
+            print("\n")
+            print("******* Invalid choice. Please enter 1 or 2.\n")
+
+
 def main():
     """
     Run all program functions
@@ -82,6 +112,14 @@ def main():
 
     # Print Description and purpose of the Program (in Descriptions GoogleSheet)
     print_description(program_scope)
+
+    selection = choose_questionnaire()
+    print(selection)
+
+    score = run_questionnaire(selection)
+    print("\n")
+    print(score)
+
  
     
 
